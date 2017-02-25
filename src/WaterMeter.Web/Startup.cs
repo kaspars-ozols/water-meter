fdabe64fff7f176eee504006cda21c08479a23ba
Kaspars Ozols <kaspars.ozols.private@gmail.com>
2017-02-26 00:27:35 +0200

    Initial checking based on template

---
+++ src/WaterMeter.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WaterMeter.Startup))]
+namespace WaterMeter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
